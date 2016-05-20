bd31d0fd2a2ade274dbd7b62ee1e0660c8d2b658
Ascendings <brotherballantine@gmail.com>
2016-05-20 12:55:46 -0400

    Updated script to work with new puppet agent packages

--- puppet-bootstrap.sh
+++ puppet-bootstrap.sh
@@ -1,117 +1,171 @@
 #!/bin/bash
 
-# Supports
-# RHEL/CentOS 5.8-6.x
-# Fedora 18,19,20
-# Debian 6,7
-# Ubuntu 10.04 (Lucid), 12.04 (Precise), 12.10 (Quantal), 13.04 (Raring), 13.10 (Saucy)
+# This script should support any puppet releases that are available at either apt.puppetlabs.com
+#   or at yum.puppetlabs.com for Debian- and RedHat-based systems
+#
+# This script has been tested on:
+# Ubuntu 16.04 (Xenial)
+# RHEL/CentOS 7.x
+# Fedora 23
+# Debian 8
 
 
-# Init Vars
-ENVIRONMENT="production"
+# Init variables
+AFTER_SCRIPT="" # Script to run immediately following the first puppet run
+BEFORE_SCRIPT="" # Script to run before the first puppet run
+CONF_PATH="/etc/puppetlabs/puppet/puppet.conf" # The puppet agent config file
+ENVIRONMENT="" # You can specify an environment to use; leaving this blank will use the default
+EXEC_PATH="/opt/puppetlabs/bin/puppet" # Path the puppet executable
+PKG_NAME="" # Name of the puppet package to install
+REPO="0" # Setting this to 1 will install puppet from OS's package repo
+WAITFORCERT="30" # Tells puppet to wait n number of seconds for the server certficate signing
 
-if [ $# -lt 1 ];
-then
-    echo "Usage $0 <SERVER_IP> <ENVIRONMENT> <CUSTOMSCRIPTNAME>"
-    exit 2
+# Get the options passed to the script
+while getopts ":a:b:c:e:p:rx:w:" opt; do
+  case $opt in
+    a)
+      # Set the after script
+      AFTER_SCRIPT="$OPTARG"
+      ;;
+    b)
+      # Set the before script
+      BEFORE_SCRIPT="$OPTARG"
+      ;;
+    c)
+      # Set the puppet config path
+      CONF_PATH="$OPTARG"
+      ;;
+    e)
+      # Set the puppet environment
+      ENVIRONMENT="$OPTARG"
+      ;;
+    p)
+      # Set the package name to install
+      PKG_NAME="$OPTARG"
+      ;;
+    r)
+      # Use the puppet package found in the system's repos
+      REPO="1"
+      ;;
+    x)
+      # Set the pupept executable path
+      EXEC_PATH="$OPTARG"
+      ;;
+    w)
+      # Tells puppet to not wait for cert
+      WAITFORCERT="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+
+# check the OS version only if we're not using the default repo version
+if [ "$REPO" != "1" ]; then
+  # Set the puppet executable path if it's not already set
+  if [ "$EXEC_PATH" != "" ]; then
+    EXEC_PATH="/opt/puppetlabs/bin/puppet"
+  fi
+  # Set the puppet package name if it's not already set
+  if [ "$PKG_NAME" != "" ]; then
+    PKG_NAME="puppet-agent"
+  fi
+
+  # Determine version, update software, install puppet
+  if [ -f /etc/lsb-release ]; then
+      . /etc/lsb-release
+      OS=$DISTRIB_ID
+      VER=$DISTRIB_RELEASE
+      SUB=$DISTRIB_CODENAME
+
+      # Ubuntu update & puppet install
+      wget https://apt.puppetlabs.com/puppetlabs-release-pc1-$SUB.deb
+      dpkg -i puppetlabs-release-pc1-$SUB.deb
+      apt-get update
+      apt-get -y install puppet-agent
+  elif [ -f /etc/debian_version ]; then
+      VER=$(cat /etc/debian_version | cut -d '.' -f 1)
+      if [ $VER = 8 ]; then
+        SUB="jessie"
+      elif [ $VER = 7 ]; then
+        SUB="wheezy"
+      fi
+
+      # Debian update & puppet install
+      if [ SUB ]; then
+        wget https://apt.puppetlabs.com/puppetlabs-release-pc1-$SUB.deb
+        dpkg -i puppetlabs-release-pc1-$SUB.deb
+        apt-get update
+        apt-get -y install puppet-agent
+      fi
+  elif [ -f /etc/fedora-release ]; then
+      OS=fedora
+      VER=$(awk '/release/ {split($3,a,"."); print a[1];}' /etc/fedora-release)
+
+      # Fedora update & puppet install
+      wget https://yum.puppetlabs.com/puppetlabs-release-pc1-fedora-$VER.noarch.rpm
+      rpm -Uvh puppetlabs-release-pc1-fedora-$VER.noarch.rpm
+      yum update -y
+      yum install -y puppet-agent
+  elif [ -f /etc/redhat-release ]; then
+      OS=rhel
+      VER=$(awk '/release/ {split($3,a,"."); print a[1];}' /etc/redhat-release)
+
+      # RHEL CentOS update & puppet install
+      wget https://yum.puppetlabs.com/puppetlabs-release-pc1-el-$VER.noarch.rpm
+      rpm -Uvh puppetlabs-release-pc1-el-$VER.noarch.rpm
+      yum update -y
+      yum install -y puppet-agent
+  fi
 else
-    SERVER_IP=$1
+  # Set the puppet executable path if it's not already set
+  if [ "$EXEC_PATH" != "" ]; then
+    EXEC_PATH="/usr/bin/puppet"
+  fi
+  # Set the puppet package name if it's not already set
+  if [ "$PKG_NAME" != "" ]; then
+    PKG_NAME="puppet"
+  fi
 fi
 
-if [ $2 ];
-then
-    ENVIRONMENT=$2
-fi
 
-# Determain CPU
-CPU=`uname -p`
-
-# Determain version, update software, install puppet 
-if [ -f /etc/debian_version ]; then
-    OS=debian
-    VER=$(cat /etc/debian_version | cut -d '.' -f 1)
-    if [ $VER = 7 ]; then
-	SUB="wheezy"
-    elif [ $VER = 6 ]; then
-	SUB="squeeze"
-    fi
-    
-    # Debian update & puppet install
-    if [ SUB ]; then
-	wget https://apt.puppetlabs.com/puppetlabs-release-$SUB.deb
-	dpkg -i puppetlabs-release-$SUB.deb
-	apt-get update
-	apt-get -y install puppet
-    fi
-	
-elif [ -f /etc/fedora-release ]; then
-    OS=fedora
-    VER=$(awk '/release/ {split($3,a,"."); print a[1];}' /etc/fedora-release)
-
-    if [ $VER = "18" ]; then
-	SUB=7
-    elif [ $VER = "19" ]; then
-	SUB=2
-    elif [ $VER = "20" ]; then
-	SUB=1
-    fi
-
-    # Fedora update & puppet install
-    wget https://yum.puppetlabs.com/fedora/f$VER/products/$CPU/puppetlabs-release-$VER-$SUB.noarch.rpm
-    rpm -Uvh puppetlabs-release-$VER-$SUB.noarch.rpm
-    yum update -y
-    yum install -y puppet
-
-elif [ -f /etc/redhat-release ]; then
-    OS=rhel
-    VER=$(awk '/release/ {split($3,a,"."); print a[1];}' /etc/redhat-release)
-
-    # RHEL CentOS update & puppet install
-    wget https://yum.puppetlabs.com/el/5/products/$CPU/puppetlabs-release-$VER-7.noarch.rpm
-    rpm -Uvh puppetlabs-release-$VER-7.noarch.rpm
-    yum update -y
-    yum install -y puppet
-
-elif [ -f /etc/lsb-release && !$OS ]; then
-    . /etc/lsb-release
-    OS=$DISTRIB_ID
-    VER=$DISTRIB_RELEASE
-    SUB=$DISTRIB_CODENAME
-
-    # Ubuntu update & puppet install
-    wget https://apt.puppetlabs.com/puppetlabs-release-$SUB.deb
-    dpkg -i puppetlabs-release-$SUB.deb
-    apt-get update
-    apt-get -y install puppet
-fi
-
-# update hosts to include puppet master server
-cat >> /etc/hosts <<EOF
-$SERVER_IP puppet
-EOF
-
-# Configure Puppet to talk to our environment
-cat >> /etc/puppet/puppet.conf <<EOF
+# Configure Puppet to talk to our puppet server
+cat >> "$CONF_PATH" <<EOF
 [agent]
-environment = $ENVIRONMENT
+server = $PUPPET_SERVER
 EOF
-
-# set puppet service defaults
-cat > /etc/default/puppet <<EOF
-# Defaults for puppet - sourced by /etc/init.d/puppet
-# Start puppet on boot?
-START=NO
-# Startup options
-DAEMON_OPTS="--environment=$ENVIRONMENT"
-EOF
+# Append the environment to the puppet config file if it's specified
+if [ "$ENVIRONMENT" != "" ]; then
+  echo "environment = $ENVIRONMENT" >> "$CONF_PATH"
+fi
 
 # Run any custom init script before starting puppet
-if [ -f $3 ];
-then
-    . $3
+if [ -f "$BEFORE_SCRIPT" && "$BEFORE_SCRIPT" != "" ]; then
+  . "$BEFORE_SCRIPT"
 fi
 
-# Start puppet agent
-/usr/bin/puppet agent --environment $ENVIRONMENT
+# Enable the puppet agent
+"$EXEC_PATH" agent --enable
+
+# Build our puppet command
+PUPPET_CMD="$EXEC_PATH agent -t "
+if [ "$WAITFORCERT" != "0" ]; then
+  PUPPET_CMD="$PUPPET_CMD -w $WAITFORCERT "
+fi
+
+# Run the puppet agent
+"$PUPPET_CMD"
+
+# Run any custom script after starting puppet
+if [ -f "$AFTER_SCRIPT" && "$AFTER_SCRIPT" != "" ]; then
+  . "$AFTER_SCRIPT"
+fi
 
 exit 0
